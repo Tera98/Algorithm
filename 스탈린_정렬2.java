8eff9cc8b2d242f8dee70fbd5cf7a282aff415da
Tera98 <117631929+Tera98@users.noreply.github.com>
2023-01-03 16:34:39 +0900

    230103

--- 스탈린_정렬2.java
+++ 스탈린_정렬2.java
@@ -1,14 +1,11 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Solution {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         
         int t = Integer.parseInt(br.readLine());
         
@@ -38,6 +35,7 @@ public class Solution {
                 System.out.print(array[j] == 0 ? "" : array[j] + " ");
             }
             System.out.println();
+            br.close();
         }
     }
 }
