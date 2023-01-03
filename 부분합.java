e2ecd4c55a20879d0b517d2a0cf8b1d6e8a4102b
Tera98 <117631929+Tera98@users.noreply.github.com>
2023-01-03 16:26:43 +0900

    230103

--- 부분합.java
+++ 부분합.java
@@ -1,14 +1,11 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Solution {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(br.readLine());
         String inNumber = br.readLine();
@@ -35,7 +32,6 @@ public class Solution {
             System.out.println(count);
             count = 0;
         }
-        bw.flush();
-        bw.close(); 
+        br.close();
     }
 }
