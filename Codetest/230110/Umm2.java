252340f5dbf23b02093097a0c4333bef0d545abd
Tera98 <117631929+Tera98@users.noreply.github.com>
2023-01-10 15:44:12 +0900

    Create Umm2.java
    시간 초과

---
+++ Codetest/230110/Umm2.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;   // 시간 복잡도를 줄여야 함
+
+public class Solution {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int testNum = sc.nextInt();
+
+        for (int i = 0; i < testNum; i++) {
+
+            int lenString = sc.nextInt();
+            int tryCount = sc.nextInt();
+            String str = sc.next();
+
+            for (int j = 0; j < tryCount; j++) {
+
+                int index1 = sc.nextInt();
+                int index2 = sc.nextInt();
+                String targetStr = str.substring(index1 - 1, index2);
+                boolean output = false;
+                int k = 1, count = 0;
+
+                if (targetStr.charAt(0) == 'U') output = true;
+
+                while (k < targetStr.length() && output) {
+                    if (targetStr.charAt(k) != 'm') {
+                        output = false;
+                    } else count += 1;
+                    k++;
+                }
+                System.out.print(output && count >= 2 ? "1" : "0");
+            }
+            System.out.println();
+        }
+    }
+}
