47d2471338131e7a07aaf70a688d83dc5cfecc2c
Tera98 <117631929+Tera98@users.noreply.github.com>
2023-01-16 10:17:06 +0900

    Create 준식이의_모험.java
    2 차원 map 구현

---
+++ Codetest/230116/준식이의_모험.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int t = Integer.parseInt(br.readLine());
+        List<String> output = new ArrayList<>();
+
+        for (int i = 0; i < t; i++) {
+
+            String str = br.readLine();
+            StringTokenizer st = new StringTokenizer(str);
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            List<String> list = new ArrayList<>();
+            for (int j = 0; j < n; j++) list.add(br.readLine());
+            int k = Integer.parseInt(br.readLine());
+            String input = br.readLine();
+            int startX = 0, startY = 0;
+
+            for (int j = 0; j < n; j++) {
+                if (list.get(j).contains("@")) {
+                    startX = list.get(j).indexOf("@") + 1;
+                    startY = j + 1;
+                }
+            }
+            for (int j = 0; j < k; j++) {
+                if (input.charAt(j) == 'L' && startX > 1) {
+                    if (list.get(startY - 1).charAt(startX - 2) != '#') startX -= 1;
+                } else if (input.charAt(j) == 'R' && startX < m) {
+                    if (list.get(startY - 1).charAt(startX) != '#') startX += 1;
+                } else if (input.charAt(j) == 'U' && startY > 1) {
+                    if (list.get(startY - 2).charAt(startX - 1) != '#') startY -= 1;
+                } else if (input.charAt(j) == 'D' && startY < n) {
+                    if (list.get(startY).charAt(startX - 1) != '#') startY += 1;
+                }
+            }
+            output.add(String.valueOf(startY) + " " + String.valueOf(startX));
+        }
+        for (int i = 0; i < t; i++) {
+            bw.write(output.get(i) + "\n");
+            bw.flush();
+        }
+        bw.close();
+    }
+}
