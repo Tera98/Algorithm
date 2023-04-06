6938c52356d647bfd29ab8878e921cc1032f2e7a
WonyongAn <awy0626@gmail.com>
2023-04-06 15:49:58 +0900

    [4/6] DS 전공 실습

---
+++ DS/4. 트리/main_heap_link.py
@@ -0,0 +1,12 @@
+from binary_link import Bheap_link
+
+b = Bheap_link()
+b.insert([90, 'watermelon'])
+b.insert([80, 'melon'])
+b.insert([60, 'apple'])
+b.insert([40, 'pear'])
+b.insert([50, 'kiwi'])
+
+
+b.print_list()
+
